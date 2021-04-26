23a6887a840399dbb7c51ec2c20e9975ed869d30
bharath2k1 <bharathreddy8345@gmail.com>
2021-04-26 07:20:35 +0530

    added table on product page

--- src/Components/Products/ProductItems/ProductComponent.jsx
+++ src/Components/Products/ProductItems/ProductComponent.jsx
@@ -7,9 +7,30 @@ import sodiumBentonite2 from "../../../Images/sodiumBentonite2.jpg";
 import sodiumBentonite3 from "../../../Images/sodiumBentonite3.jpg";
 import sodiumBentonite4 from "../../../Images/sodiumBentonite4.jpg";
 import BannerComponent from "../Banner/BannerComponent";
-import { Table } from "react-bootstrap";
+// import { Table } from "react-bootstrap";
 import BasicTable from "./BasicTable";
+import MediaCard from "../MediaCard/MediaCard";
+import Carousel from "react-multi-carousel";
 
+
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 4,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+};
 
 class ProductComponent extends React.Component {
   state = {
@@ -63,59 +84,90 @@ class ProductComponent extends React.Component {
 
   render() {
     const { products, index } = this.state;
-    
+
     // console.log(index);s
 
     return (
       <div>
-        {products.map((item) => (
-          <BannerComponent title={item.title} text={item.description} />
-        ))}
+        <div>
+          {products.map((item) => (
+            <BannerComponent title={item.title} text={item.description} />
+          ))}
 
-        {products.map((item) => (
-          <div className="details" key={item._id}>
-            <div>
-              <div className="big-img">
-                <img src={item.src[index]} alt="sodium bentomite" />
-                <DetailsThumb
-                  images={item.src}
-                  tab={this.handleTab}
-                  myRef={this.myRef}
+          {products.map((item) => (
+            <div className="details" key={item._id}>
+              <div>
+                <div className="big-img">
+                  <img src={item.src[index]} alt="sodium bentomite" />
+                  <DetailsThumb
+                    images={item.src}
+                    tab={this.handleTab}
+                    myRef={this.myRef}
+                  />
+                </div>
+              </div>
+
+              <div className="box1">
+                <div className="row">
+                  <h2>{item.title}</h2>
+                  <span>${item.price}</span>
+                </div>
+
+                <BasicTable
+                  key={item.id}
+                  specifications={item.specifications}
                 />
+
+                <p>{item.description}</p>
+                <p>{item.content}</p>
+
+                <a class="btn" href="">
+                  Yes! I'm interested
+                </a>
               </div>
             </div>
+          ))}
+        </div>
+        <Carousel
+            containerClass="carousel-container"
+            responsive={responsive}
             
-
-            <div className="box1">
-              <div className="row">
-                <h2>{item.title}</h2>
-                <span>${item.price}</span>
-              </div>
- 
-
-
-            <BasicTable key={item.id} specifications={item.specifications}/>
-           
-             
-
-              <p>{item.description}</p>
-              <p>{item.content}</p>
-
-              <a class="btn" href="">
-                Yes! I'm interested
-              </a>
-            </div>
-          </div>
-        ))}
+            showDots={false}
+            responsive={responsive}
+            ssr={true} // means to render carousel on server-side.
+            infinite={true}
+            // autoPlay={this.props.deviceType !== "mobile" ? true : false}
+            autoPlay={true}
+            autoPlaySpeed={3000}
+            keyBoardControl={true}
+            customTransition="transform 500ms ease-in-out"
+            transitionDuration={500}
+            
+            removeArrowOnDeviceType={["tablet", "mobile"]}
+            // deviceType={this.props.deviceType}
+            dotListClass="custom-dot-list-style"
+            itemClass="carousel-item-padding-40-px"
+          >
+              
+              <MediaCard/>
+              <MediaCard/>
+              <MediaCard/>
+              <MediaCard/>
+              <MediaCard/>
+              <MediaCard/>
+              <MediaCard/>
+              <MediaCard/>
+              <MediaCard/>
+              <MediaCard/>
+              <MediaCard/>
+              <MediaCard/>
+              <MediaCard/>
+            
+          </Carousel>
+        
       </div>
     );
   }
 }
 
 export default ProductComponent;
-
-
-
-
-
-
